12f9b222c88b1776794980527e4d776fce1e2f72
Giulia Gallo <giulia.gallo2903@gmail.com>
2025-01-17 13:23:31 +0100

    punto 1 e 2

--- src/main/java/org/example/Archivio.java
+++ src/main/java/org/example/Archivio.java
@@ -1,14 +1,28 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Archivio {
     //aggiungere un elemento che viene di conseguenza salvato in una lista
-    //creo la lista che è un array
-    List<Catalogo> archivio = new ArrayList<>();
+    //creo la lista che è un array usando Hashset per evitare duplicati
+    Set<Catalogo> archivio = new HashSet<>();
     //ora genero il metodo per aggiungere un nuovo elemento
-    public void aggiuntaElemento(Catalogo elemento) { // il primo parametro rappresenta l'oggetto che vogliamo passare
-        archivio.add(elemento);
+    public boolean aggiuntaElemento(Catalogo elemento) { // il primo parametro rappresenta l'oggetto che vogliamo passare
+      return  archivio.add(elemento);//se l'elemento è nuovo restituisce true e viene aggiunto
+    }
+    public class ElementoNonTrovatoException extends Exception{
+        public ElementoNonTrovatoException(String msg) {
+            super(msg);
+        }
+
+    }
+    public Catalogo ricercaElemento(String isbn) throws ElementoNonTrovatoException {
+      for (Catalogo elemento : archivio) {
+          if (elemento.getIsbn().equals(isbn)) {
+              return elemento;
+          }
+      }
+        throw new ElementoNonTrovatoException ("Elemento non trovato");
     }
 }
